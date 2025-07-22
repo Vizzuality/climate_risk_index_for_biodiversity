c15e14fa76adfdfdde10bbcf3dc581a0550e9360
Andrés González <999124+agnlez@users.noreply.github.com>
2025-07-22 12:39:18 +0200

    [CRIB-35] atlantic bioregions layer (#6)

--- client/src/containers/map/layer-manager/index.tsx
+++ client/src/containers/map/layer-manager/index.tsx
@@ -4,6 +4,7 @@ import WDPALayer from "@/containers/map/layers/wdpa";
 import { useScenario } from "@/store";
 import LowEmissionsLayer from "@/containers/map/layers/low-emissions";
 import HighEmissionsLayer from "@/containers/map/layers/high-emissions";
+import AtlanticBioregionsLayer from "@/containers/map/layers/atlantic-bioregions";
 
 export default function LayerManager() {
   const [scenario] = useScenario();
@@ -11,6 +12,7 @@ export default function LayerManager() {
   return (
     <>
       <WDPALayer />
+      <AtlanticBioregionsLayer />
       {scenario === "high" && <HighEmissionsLayer />}
       {scenario === "low" && <LowEmissionsLayer />}
     </>
